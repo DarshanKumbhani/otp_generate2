d3d00174f3f8dad0318e6a3c48e5137e9974cf8d
userdixit <120080979+userdixit@users.noreply.github.com>
2023-03-01 09:54:41 +0530

    g

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,170 +1,139 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+
 class Home_screen extends StatefulWidget {
   const Home_screen({Key? key}) : super(key: key);
 
   @override
   State<Home_screen> createState() => _Home_screenState();
-
-
 }
-List col = [];
 
 
-int p=0,q=0,r=0,s=0,t=0,u=0;
-
-
-int? num ;
-
-List c2 = [];
-List c1 = [
-  Colors.pink,
-  Colors.pinkAccent,
-  Colors.red,
-  Colors.greenAccent,
-  Colors.deepOrangeAccent,
-  Colors.deepOrangeAccent,
-  Colors.teal,
-  Colors.amberAccent,
-  Colors.lime,
-  Colors.lightBlueAccent,
-  Colors.redAccent,
-  Colors.cyan
-];
 
 class _Home_screenState extends State<Home_screen> {
-
-
+  List colore = [
+    Colors.purple,
+    Colors.lightGreenAccent,
+    Colors.lightGreen,
+    Colors.grey,
+    Colors.green,
+    Colors.deepPurple,
+    Colors.deepPurpleAccent,
+    Colors.purpleAccent,
+    Colors.redAccent,
+    Colors.pink,
+    Colors.amberAccent,
+    Colors.lime,
+    Colors.indigo,
+    Colors.teal,
+    Colors.indigoAccent,
+    Colors.tealAccent
+  ];
+  int plate = 0;
+  int plate2 = 1;
+  int plate3 = 2;
+  int plate4 = 3;
+  int plate5 = 4;
+  int plate6 = 5;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Color Palette"),
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent,
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "Color Plaette\n",
-                    style: TextStyle(
-                      color: Color(0xff3BB3F9),
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  TextSpan(
-                    text: "   Generator",
-                    style: TextStyle(
-                      color: Color(0xff3BB3F9),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
+            Text(
+              "Color Palette\n   Generator",
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF3BB3F9)
+              ),
+            ),
+            SizedBox(height: 100,),
+            Container(
+                height: 60,
+                width: 100,
+                decoration: BoxDecoration(
+                    color: colore[plate],
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(21),
+                        topRight: Radius.circular(21)
                     )
-                  )
-                ]
-              ),
+                )
             ),
-
-            SizedBox(height: 20,),
             Container(
-              width: 80,
-              height: 200,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color: Colors.teal,border: Border.all(color: Colors.black)),
-              child: Column(
-                //crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-
-
-                  plate(c1[p]),
-                  plate(c1[q]),
-                  plate(c1[r]),
-                  plate(c1[s]),
-                  plate(c1[t]),
-                  plate(c1[u]),
-                ],
-              ),
+              height: 60,
+              width: 100,
+              color: colore[plate2],
             ),
-          GestureDetector(onTap: () {
-          setState(() {
-          Random rand = Random();
-           p = rand.nextInt(12);
-
-          q = rand.nextInt(12);
-         while(q == p)
-         {
-           q = rand.nextInt(12);
-         };
-
-          r = rand.nextInt(12);
-          while(r == p || r==q)
-          {
-          r = rand.nextInt(12);
-          };
-
-          s = rand.nextInt(12);
-          while(s == p || s==q || s==r)
-          {
-          s = rand.nextInt(12);
-          };
-
-          s = rand.nextInt(12);
-          while(e == p || e==q || e==r || e==s)
-          {
-            s = rand.nextInt(12);
-          };
-
-          u = rand.nextInt(12);
-          while(u == p || u==q || u==r || u==s || u==e);
-          {
-            u = rand.nextInt(12);
-          };
-          c2 = [p,q,r,s,t,u];
-
-
-          print ("$c2 $p $q $r $s $t $u");
-
-
-          });
-
-
-
-          },
-
-          ),
-
             Container(
-              height: 40,
-              width: 150,
-              decoration: BoxDecoration(
-                  border: Border.all(width: 2,color: Color(0xff3BB3F9),
-                  )
-              ),
-              alignment: Alignment.center,
-              child: Text(
-                "Generate",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Color(0xff3BB3F9)
+              height: 60,
+              width: 100,
+              color: colore[plate3],
+            ),
+            Container(
+              height: 60,
+              width: 100,
+              color: colore[plate4],
+            ),
+            Container(
+              height: 60,
+              width: 100,
+              color: colore[plate5],
+            ),
+            Container(
+                height: 60,
+                width: 100,
+                decoration: BoxDecoration(
+                    color: colore[plate6],
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(21),
+                        bottomRight: Radius.circular(21)
+                    )
+                )
+            ),
+            SizedBox(height: 60,),
+            InkWell(
+              onTap: (){
+                var co = Random();
+                setState(() {
+                  plate = co.nextInt(10);
+                  plate2 = co.nextInt(10);
+                  plate3 = co.nextInt(10);
+                  plate4 = co.nextInt(10);
+                  plate5 = co.nextInt(10);
+                  plate6 = co.nextInt(10);
+                });
+              },
+              child: Container(
+                height: 46,
+                width: 215,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: Color(0xFF3BB3F9,),width: 3)
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  "Generate",
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF3BB3F9,)
+                  ),
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
-      backgroundColor: Color(0xffFFFFFF),
-    );
-  }
-  Widget plate(Color c1)
-  {
-    return Container(
-      height: 20,
-      width: 60,
-      color: c1,
     );
   }
 }
